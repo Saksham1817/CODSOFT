ac3db7aeb46bbdd25e389256b5c26d675822e13c
saksham <132780737+Saksham1817@users.noreply.github.com>
2024-05-12 14:58:27 +0000

    push

--- codesoft AI internship Task 1 ChatBot/chatbot.py
+++ codesoft AI internship Task 1 ChatBot/chatbot.py
@@ -1,14 +1,16 @@
 import re
 from datetime import datetime
 
-def simplechatbot(user_input):
+def simple_chatbot(user_input):
     responses = {
         r'\bhello\b': "Hello! How can I help you?",
         r'\bwho are you\b': "I'm a chatbot, and I'm here to assist you!",
-        r'\bwho is your boss\b': "SAKSHAM is my boss!",
+        r'\bwho is your boss\b': "Saksham is my boss!",
+        r'\bwhat can you do here\b': "I'm a chatbot, and I'm here to assist you!",
         r'\bbye\b': "Goodbye! Have a great day!",
         r'\btime\b': f"The current time is {datetime.now().strftime('%H:%M')}.",
-        r'\bdate\b': f"Today's date is {datetime.now().strftime('%Y-%m-%d')}."
+        r'\bdate\b': f"Today's date is {datetime.now().strftime('%Y-%m-%d')}.",
+        r'\bquit\b': "Chatbot: Goodbye! Thanks for chatting with me."
     }
 
     for pattern, reply in responses.items():
@@ -18,15 +20,15 @@ def simplechatbot(user_input):
     return "I'm sorry, I don't understand that."
 
 def main():
-    print("Chatbot: Hello! You can type 'exit' if you want to exit.")
+    print("Chatbot: Hello! You can type 'quit' or 'exit' if you want to exit.")
 
     while True:
         user_input = input("You: ")
-        if user_input.lower() == "exit":
-            print("Chatbot: Goodbye!")
+        if user_input.lower() in ["quit", "exit"]:
+            print("Chatbot: Goodbye! Thanks for chatting with me.")
             break
 
-        response = simplechatbot(user_input)
+        response = simple_chatbot(user_input)
         print("Chatbot:", response)
 
 if __name__ == "__main__":
